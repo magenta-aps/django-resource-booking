cdbc0e3b18f6090da2baee54d3cc51c890995b83
Jørgen Ulrik B. Krag <jubk@magenta.dk>
2015-11-17 13:53:03 +0100

    - Add base for the translation system

--- booking/views.py
+++ booking/views.py
@@ -1,4 +1,8 @@
+# -*- coding: utf-8 -*-
 from django.views.generic import TemplateView
+from django.utils.translation import ugettext as _
+
+i18n_test = _("Dette tester oversættelses-systemet")
 
 
 class MainPageView(TemplateView):
