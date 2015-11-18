42d4525219d4a74f12486702cb65e558bd465141
Carsten Agger <carstena@magenta.dk>
2015-11-18 11:07:02 +0100

    Added user profiles and admin interface.

--- booking/admin.py
+++ booking/admin.py
@@ -1,3 +1,7 @@
-# from django.contrib import admin
+from django.contrib import admin
 
-# Register your models here.
+from .models import Visit, UnitType, Unit
+
+admin.site.register(Visit)
+admin.site.register(UnitType)
+admin.site.register(Unit)
