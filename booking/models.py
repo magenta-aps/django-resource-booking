b054ddc3b802cc5950d38843f7f15b2d87d3675a
Carsten Agger <carstena@magenta.dk>
2015-11-24 14:48:41 +0100

    Added most of the fields for the visits model.

--- booking/models.py
+++ booking/models.py
@@ -16,6 +16,7 @@ class Unit(models.Model):
     parent = models.ForeignKey('self', null=True, blank=True)
 
 
+# Master data related to bookable resources start here
 class Subject(models.Model):
     """A relevant subject from primary or secondary education."""
     stx = 0
@@ -38,8 +39,53 @@ class Subject(models.Model):
     description = models.TextField()
 
 
+class Link(models.Model):
+    """"An URL and relevant metadata."""
+    url = models.URLField()
+    name = models.CharField(max_length=256)
+    # Note: "description" is intended as automatic text when linking in web
+    # pages.
+    description = models.CharField(max_length=256)
+
+
+class Topic(models.Model):
+    """Tag class, just name and description fields."""
+    name = models.CharField(max_length=256)
+    description = models.TextField()
+
+
+class AdditionalService(models.Model):
+    """Additional services, i.e. tour of the place, food and drink, etc."""
+    name = models.CharField(max_length=256)
+    description = models.TextField()
+
+
+class SpecialRequirement(models.Model):
+    """Special requirements for visit - e.g., driver's license."""
+    name = models.CharField(max_length=256)
+    description = models.TextField()
+
+
+# Bookable resources
 class Resource(models.Model):
     """A bookable resource of any kind."""
+
+    # Resource type.
+    STUDENT_FOR_A_DAY = 0
+    FIXED_SCHEDULE_GROUP_VISIT = 1
+    FREELY_SCHEDULED_GROUP_VISIT = 2
+    STUDY_PROJECT = 3
+    SINGLE_EVENT = 4
+    OTHER_RESOURCES = 5
+
+    resource_type_choices = (
+        (STUDENT_FOR_A_DAY, _(u"Studerende for en dag")),
+        (FIXED_SCHEDULE_GROUP_VISIT, _(u"Gruppebesøg med faste tider")),
+        (FREELY_SCHEDULED_GROUP_VISIT, _(u"Gruppebesøg uden faste tider")),
+        (STUDY_PROJECT, _(u"Studieretningsprojekt - SRP")),
+        (SINGLE_EVENT,  _(u"Enkeltstående event")),
+        (OTHER_RESOURCES, _(u"Andre tilbud"))
+    )
 
     # Target audience choice - student or teacher.
     TEACHER = 0
@@ -55,24 +101,35 @@ class Resource(models.Model):
     SECONDARY = 1
 
     institution_choices = (
-        (PRIMARY, _(u'Folkeskole')),
+        (PRIMARY, _(u'Grundskole')),
         (SECONDARY, _(u'Gymnasium'))
     )
 
+    # Level choices - A, B or C
+    A = 0
+    B = 0
+    C = 0
+
+    level_choices = (
+        (A, u'a'), (B, u'B'), (C, u'C')
+    )
+
+    type = models.IntegerField(choices=resource_type_choices)
     title = models.CharField(max_length=256)
     description = models.TextField()
     mouseover_description = models.CharField(max_length=512)
     unit = models.ForeignKey(Unit, null=True, blank=True)
+    links = models.ManyToManyField(Link)
     audience = models.IntegerField(choices=audience_choices,
                                    verbose_name=_(u'Målgruppe'))
     institution_level = models.IntegerField(choices=institution_choices,
                                             verbose_name=_(u'Institution'),
                                             default=SECONDARY)
     subjects = models.ManyToManyField(Subject)
-    # level = <<suitable choice list for levels>>
-    # class_year = <<suitable representation, maybe free text.>>
+    level = models.IntegerField(choices=level_choices,
+                                verbose_name=_(u"Niveau"))
     # tags = <<choice list for tags>>
-    # topics = <<choice list for topics>>
+    topics = models.ManyToManyField(Topic)
 
     # Comment field for internal use in backend.
     comment = models.TextField()
@@ -80,17 +137,18 @@ class Resource(models.Model):
 
 class Visit(Resource):
     """A bookable visit of any kind."""
-
-    # Visit type choice - TBS
-    # TODO: Find out what types we want here!
-    VISIT_TYPE_1 = 0
-    VISIT_TYPE_2 = 1
-
-    visit_type_choices = (
-        (VISIT_TYPE_1, _("Type 1")),
-        (VISIT_TYPE_2, _("Type 2"))
+    # locality = <<suitable representation of locality.>>
+    # room = <<representation of roomm, maybe free text field.>>
+    do_send_evaluation = models.BooleanField(
+        verbose_name=_(u"Udsend evaluering"), default=False
     )
-
-#
-# Units (faculties, institutes etc)
-#
+    price = models.DecimalField(default=0)
+    additional_services = models.ManyToManyField(AdditionalService)
+    special_requirements = models.ManyToManyField(SpecialRequirement)
+    is_group_visit = models.BooleanField(default=True)
+    # Min/max number of visitors - only relevant for group visits.
+    mininimum_number_of_visitors = models.IntegerField(null=True, blank=True)
+    maximum_number_of_visitors = models.IntegerField(null=True, blank=True)
+    do_create_waiting_list = models.BooleanField(default=False)
+    do_show_countdown = models.BooleanField(default=False)
+    # preparatory_material = <<< ... >>>
