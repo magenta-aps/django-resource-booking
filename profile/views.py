69f8059aece0d281c84fc55b9c2e86efa6753e05
Carsten Agger <agger@modspil.dk>
2015-12-16 13:51:57 +0100

    Added login requirement to personal page and introduced Nanna's markup.

--- profile/views.py
+++ profile/views.py
@@ -1,3 +1,9 @@
-# from django.shortcuts import render
 
-# Create your views here.
+from django.views.generic import TemplateView
+
+from booking.views import LoginRequiredMixin
+
+
+class ProfileView(LoginRequiredMixin, TemplateView):
+    """Display the user's profile."""
+    pass
