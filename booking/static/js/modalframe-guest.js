c17f660afd6dc45563e21cecf01a441a917285af
Lars Peter Thomsen <larsp@magenta.dk>
2016-01-21 11:09:59 +0100

    Better inter-frame communication

--- booking/static/js/modalframe-guest.js
+++ booking/static/js/modalframe-guest.js
@@ -1,4 +1,24 @@
+window.modal = {
+    parent: window.parent.modal,
+    setId: function(id) {
+        this.id = id;
+    },
+    close: function() {
+        this.parent.close(this.id);
+    },
+    setHeight: function(height) {
+        this.parent.setHeight(this.id, height);
+    }
+};
+(function(){
+    var m = /[#;]id=([^;]+)/.exec(document.location.hash);
+    if (m) {
+        modal.id = m[1];
+    }
+})();
+
+
 $(function(){
-    var pageHeight = $(document).height();
-    document.location.hash = "height="+pageHeight;
+    modal.setHeight($(document).height());
+    $("*[data-dismiss='modal']").click(modal.close.bind(modal));
 });
--- booking/static/js/modalframe-host.js
+++ booking/static/js/modalframe-host.js
@@ -1,31 +1,36 @@
+window.modal = {
+    nextId: 0,
+    modals: {},
+    close: function(id) {
+        if (modal.modals[id]) {
+            modal.modals[id].modal('hide');
+        }
+    },
+    setHeight: function(id, height) {
+        if (this.modals[id]) {
+            this.modals[id].find("iframe").css("height", height);
+        }
+    },
+    add: function(modal) {
+        var id = modal.attr("id") || ("m" + window.modalId++);
+        this.modals[id] = modal;
+        return id;
+    }
+};
+
+
 $.fn.extend({
     'hostModalIframe': function(url) {
-        this.on('shown.bs.modal', function() {
+
+        var id = modal.add(this);
+        url += ((url.indexOf("#")==-1) ? "#":";" ) + "id=" + id;
+
+        this.on('shown.bs.modal', function () {
             var container = $(this).find(".modal-content");
             if (!container.find("iframe").length) {
                 var iframe = $("<iframe>");
                 iframe.attr("src", url);
                 container.append(iframe);
-                iframe.load(function () {
-                    var hash = iframe.get(0).contentDocument.location.hash;
-                    if (hash && hash[0] == "#") {
-                        /*var hashParts = hash.substr(1).split(";");
-                         for (var i=0; i<hashParts.length; i++) {
-                         var kvp = hashParts[i].split("=");
-                         if (kvp.length == 2) {
-                         var key = kvp[0],
-                         value = kvp[1];
-                         if (key=="height") {
-                         iframe.css("height", value);
-                         }
-                         }
-                         }*/
-                        var m = /[#;]height=([\d]+)/.exec(hash);
-                        if (m) {
-                            iframe.css("height", m[1]);
-                        }
-                    }
-                });
             }
         });
     }
