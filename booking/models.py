baa43477275807a14bd8769626af497332795f4f
Lars Peter Thomsen <larsp@magenta.dk>
2016-02-04 15:07:24 +0100

    Require level field

--- booking/models.py
+++ booking/models.py
@@ -1017,7 +1017,7 @@ class Booker(models.Model):
     )
     level = models.IntegerField(
         choices=level_choices,
-        blank=True,
+        blank=False,
         verbose_name=u'Niveau'
     )
 
