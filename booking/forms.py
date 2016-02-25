41c06e1ea0e07f7be1601527a5773b4329017c46
Lars Peter Thomsen <larsp@magenta.dk>
2016-02-25 13:05:56 +0100

    Put a css class on subject field

--- booking/forms.py
+++ booking/forms.py
@@ -472,5 +472,8 @@ class EmailComposeForm(BaseEmailComposeForm):
 
     subject = forms.CharField(
         max_length=77,
-        label=_(u'Emne')
+        label=_(u'Emne'),
+        widget=TextInput(attrs={
+            'class': 'form-control'
+        })
     )
