730a14abb1b8cdf720b02dc40d52ac1b3cf3e1cd
Jørgen Ulrik B. Krag <jubk@magenta.dk>
2016-04-07 09:03:45 +0200

    - Flake 8

--- booking/views.py
+++ booking/views.py
@@ -2853,13 +2853,14 @@ class EmailReplyView(DetailView):
 
     def get_occurrence(self):
         occ = None
+
         try:
             ct = ContentType.objects.get(pk=self.object.content_type_id)
             if ct.model_class() == VisitOccurrence:
                 occ = ct.get_object_for_this_type(pk=self.object.object_id)
         except Exception as e:
             print "Error when getting email-reply object: %s" % e
-        
+
         return occ
 
     def get_context_data(self, **kwargs):
