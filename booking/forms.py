338504c115f5b58db8b268b7ec66dc9ca64a1cbe
Lars Peter Thomsen <larsp@magenta.dk>
2016-05-09 12:10:02 +0200

    Update max value of attendees field based on available seats

--- booking/forms.py
+++ booking/forms.py
@@ -634,15 +634,21 @@ class BookingForm(forms.ModelForm):
             len(visit.future_events) > 0
         )
         if self.scheduled:
-            self.fields['visitoccurrence'].choices = (
-                (
-                    x.pk,
-                    formats.date_format(x.start_datetime, "DATETIME_FORMAT") +
-                    " " + _("(%d pladser)") % x.available_seats()
-                )
-                for x in visit.future_events.order_by('start_datetime')
-                if x.available_seats() > 0
-            )
+            choices = []
+            for x in visit.future_events.order_by('start_datetime'):
+                available_seats = x.available_seats()
+                date = formats.date_format(x.start_datetime, "DATETIME_FORMAT")
+                if available_seats is None:
+                    choices.append((x.pk, date))
+                elif available_seats > 0:
+                    choices.append(
+                        (x.pk,
+                         date + " " +
+                         _("(%d pladser tilbage)") % available_seats
+                        )
+                    )
+
+            self.fields['visitoccurrence'].choices = choices
             self.fields['visitoccurrence'].required = True
 
 
