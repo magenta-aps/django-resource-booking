4c05f7b537b7ba26ae08a7e3c72bcae7bf319566
Lars Peter Thomsen <larsp@magenta.dk>
2016-09-06 16:45:57 +0200

    First skeleton forms

--- booking/resource_based/forms.py
+++ booking/resource_based/forms.py
@@ -5,3 +5,40 @@ from booking.models import ResourceType
 from booking.models import TeacherResource
 from django import forms
 from django.utils.translation import ugettext_lazy as _
+
+
+
+class CreateResourceForm(forms.Form):
+    type = forms.ChoiceField(
+        label=_(u'Type'),
+        choices=[
+            (x.id, x.name)
+            for x in ResourceType.objects.all()
+            ],
+        required=True
+    )
+    unit = forms.ChoiceField(
+        label=_(u'Enhed'),
+        choices=[
+            (x.id, x.name)
+            for x in OrganizationalUnit.objects.all()
+            ]
+    )
+
+class EditTeacherResourceForm(forms.Modelform):
+    class Meta:
+        model = TeacherResource
+        fields = ('user', 'type', 'unit')
+        widgets = {
+            'type': forms.HiddenInput(),
+            'unit': forms.HiddenInput()
+        }
+
+    def __init__(self, *args, **kwargs):
+        self.instance = kwargs.get('instance')
+        if not self.instance.pk:
+            if 'initial' not in kwargs:
+                kwargs['initial'] = {}
+            if 'unit' in kwargs:
+                kwargs['initial']['unit'] = kwargs['unit']
+        super(EditTeacherResourceForm, self).__init__(*args, **kwargs)
--- booking/resource_based/views.py
+++ booking/resource_based/views.py
@@ -1,2 +1,23 @@
 # encoding: utf-8
+from django.core.urlresolvers import reverse, reverse_lazy
+from django.shortcuts import render, redirect
+from django.views.generic.edit import UpdateView, FormMixin, DeleteView, FormView
+from booking.resource_based.forms import CreateResourceForm, EditTeacherResourceForm
+from booking.resource_based.models import ResourceType
+
 import booking.models as booking_models
+
+
+class CreateResourceView(FormView):
+    template_name = "booking/accept_spot.html"
+    form_class = CreateResourceForm
+
+    def post(self, request, *args, **kwargs):
+        form = self.get_form()
+        if form.is_valid():
+            typeId = int(form.cleaned_data['type'])
+            type = ResourceType.objects.get(pk=typeId)
+            if type.name == ResourceType.default_resource_names[ResourceType.RESOURCE_TYPE_TEACHER]:
+                return redirect(
+                    reverse('teacher-resource-edit', args={'unit': int(form.cleaned_data['unit'])})
+                )
