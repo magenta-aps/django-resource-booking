f925cf480e71d2b615aaa123179adbed62012693
Jørgen Ulrik B. Krag <jubk@magenta.dk>
2016-09-29 16:27:48 +0200

    - Move auto-creation of resources for teachers and hosts to   save method of UserProfile.

--- profile/signals.py
+++ profile/signals.py
@@ -35,24 +35,6 @@ def custom_update_user(sender, **kwargs):
         )
         profile.save()
 
-        # Create a resource for the user
-        if profile.is_teacher and group is not None:
-            resource = booking_models.TeacherResource(
-                user=user,
-                organizationalunit=group
-            )
-            resource.make_calendar()
-            resource.save()
-
-        # Create a resource for the user
-        if profile.is_host and group is not None:
-            resource = booking_models.HostResource(
-                user=user,
-                organizationalunit=group
-            )
-            resource.make_calendar()
-            resource.save()
-
     except Exception as e:
         print "Error during user autogeneration: %s" % e
 
