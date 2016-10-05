1e5aae1e7941430f73e0ed39db6b859140327043
Jørgen Ulrik B. Krag <jubk@magenta.dk>
2016-10-05 15:06:21 +0200

    - Only include actually bookable products on the frontpage list   if accessed by a not-logged-in user.

--- booking/views.py
+++ booking/views.py
@@ -122,13 +122,40 @@ class MainPageView(TemplateView):
     template_name = 'frontpage.html'
 
     def get_context_data(self, **kwargs):
+        updated_qs = Product.get_latest_updated()
+
+        if not self.request.user.is_authenticated():
+            updated_qs = updated_qs.filter(
+                Q(time_mode=Product.TIME_MODE_GUEST_SUGGESTED) |
+                Q(
+                    # Only stuff that can be booked
+                    eventtime__bookable=True,
+                    # In the future
+                    eventtime__start__gt=timezone.now(),
+                    # Only include stuff with bookable states
+                    eventtime__visit__workflow_status__in=(
+                        Visit.BOOKABLE_STATES
+                    ),
+                ) & Q(
+                    # Either not resource controlled
+                    (~Q(time_mode=Product.TIME_MODE_RESOURCE_CONTROLLED)) |
+                    # Or resource-controlled with nonblocked eventtimes
+                    Q(
+                        time_mode=Product.TIME_MODE_RESOURCE_CONTROLLED,
+                        eventtime__resource_status__in=(
+                            booking_models.EventTime.NONBLOCKED_RESOURCE_STATES
+                        )
+                    )
+                )
+            )
+
         context = {
             'lists': [
                 {
                     'color': self.HEADING_GREEN,
                     'type': 'Product',
                     'title': _(u'Senest opdaterede tilbud'),
-                    'queryset': Product.get_latest_updated(),
+                    'queryset': updated_qs,
                     'limit': 10,
                     'button': {
                         'text': _(u'Vis alle'),
@@ -723,16 +750,13 @@ class SearchView(BreadcrumbMixin, ListView):
 
                 eventtime_cls = booking_models.EventTime
 
-                nonblocked_statuses = [
-                    x[0] for x in eventtime_cls.resource_status_choices
-                    if x[0] != eventtime_cls.RESOURCE_STATUS_BLOCKED
-                ]
+                nonblocked = eventtime_cls.NONBLOCKED_RESOURCE_STATES
 
                 date_cond = date_cond & Q(
                     (~Q(time_mode=res_controlled)) |
                     Q(
                         time_mode=res_controlled,
-                        eventtime__resource_status__in=nonblocked_statuses
+                        eventtime__resource_status__in=nonblocked
                     )
                 )
 
