7abfb345031ce27f6e5e16103c2b177702cf7413
Jørgen Ulrik B. Krag <jubk@magenta.dk>
2017-10-02 12:59:14 +0200

    Prevent MSIE from doing double submits on modal booking forms

--- booking/static/js/ieform.js
+++ booking/static/js/ieform.js
@@ -1,7 +1,10 @@
 $(function(){
     // Because IE doesn't support <button form="">
-    $("button[form]").click(function(){
+    $("button[form]").click(function(e) {
         $("form#" + $(this).attr("form")).submit();
+        // Make sure to prevent default behaviors, so we do not get two submits
+        e.preventDefault();
+        return false;
     });
 
     // Because IE doesn't support hiding <option>
