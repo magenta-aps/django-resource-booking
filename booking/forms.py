931a6f98f53036968c2e3af60ca957bc583238d3
Jørgen Ulrik B. Krag <jubk@magenta.dk>
2017-10-12 16:57:01 +0200

    Automatically set attendee count to 1 on student for a day

--- booking/forms.py
+++ booking/forms.py
@@ -1002,6 +1002,8 @@ class BookerForm(forms.ModelForm):
                 # Class level is not mandatory for teacher events.
                 if product.type == Product.TEACHER_EVENT:
                     self.fields['level'].required = False
+                if product.type == Product.STUDENT_FOR_A_DAY:
+                    self.fields['attendee_count'].initial = 1
 
         # Eventually we may want a prettier solution,
         # but for now this will have to do
