768291bb29aec050aea748fe47db6674d2b79a79
Lars Peter Thomsen <larsp@magenta.dk>
2018-01-26 09:53:50 +0100

    booking/views.py

--- booking/views.py
+++ booking/views.py
@@ -1632,7 +1632,9 @@ class EditProductView(BreadcrumbMixin, EditProductBaseView):
 
         if self.request.method == 'POST':
             forms['autosendformset'] = ProductAutosendFormSet(
-                self.request.POST, instance=self.object
+                self.request.POST,
+                instance=self.object,
+                initial=self.get_initial_autosends()
             )
         return forms
 
