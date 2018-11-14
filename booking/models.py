5c92b94128d25b5926f37a7f1e528751aae4d650
Lars Peter Thomsen <larsp@magenta.dk>
2018-11-14 14:40:07 +0100

    Minor fix

--- booking/models.py
+++ booking/models.py
@@ -3935,7 +3935,7 @@ class Visit(AvailabilityUpdaterMixin, models.Model):
                                 'booking': booking,
                                 'booker': booking.booker
                             },
-                            booking.booker,
+                            KUEmailRecipient(booking.booker),
                             self,
                             unit,
                             original_from_email=reply_recipients
