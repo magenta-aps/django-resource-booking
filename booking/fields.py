1ebb273c1050aac12afb6064433f06ebd4539874
Søren Howe Gersager <thesyre@pm.me>
2019-02-08 14:43:45 +0100

    change one more migration from own DurationField to djangos DurationField

--- booking/fields.py
+++ booking/fields.py
@@ -12,43 +12,6 @@ from .widgets import OrderedMultipleHiddenChooser
 from .widgets import CheckboxSelectMultipleDisable, DurationWidget
 from .widgets import SelectDisable, SelectMultipleDisable
 
-COLUMN_TYPES = defaultdict(lambda: "char(20)")
-COLUMN_TYPES["django.db.backends.postgresql_psycopg2"] = "interval"
-COLUMN_TYPES["django.contrib.gis.db.backends.postgis"] = "interval"
-
-
-class DurationField(models.Field):
-    """
-        Store a datetime.timedelta as an INTERVAL in postgres, or a
-        CHAR(20) in other database backends.
-        """
-    _south_introspects = True
-
-    def __init__(self, labels=None, *args, **kwargs):
-        super(DurationField, self).__init__(*args, **kwargs)
-        self.labels = labels
-
-    def formfield(self, **kwargs):
-        labels = self.labels
-        kwargs['widget'] = DurationWidget(day_label=labels.get('day'),
-                                          hour_label=labels.get('hour'),
-                                          minute_label=labels.get('minute'))
-        return super(DurationField, self).formfield(**kwargs)
-
-    def get_prep_value(self, value):
-        if self.null and value == "":
-            return None
-        if (value is None) or isinstance(value, six.string_types):
-            return value
-        return str(value).replace(',', '')
-
-    # Database stuff
-
-    def db_type(self, connection):
-        return COLUMN_TYPES[connection.settings_dict['ENGINE']]
-
-    def get_db_prep_value(self, value, connection=None, prepared=None):
-        return self.get_prep_value(value)
 
 
 class ExtensibleMultipleChoiceField(MultipleChoiceField):
