dc4c0945e83191e7211df4043d454ce4e872bc6c
Søren Howe Gersager <thesyre@pm.me>
2019-02-11 12:33:12 +0100

    formatting

--- booking/utils.py
+++ booking/utils.py
@@ -61,9 +61,9 @@ def get_related_content_types(model):
         types = [ContentType.objects.get_for_model(model)]
 
         related_objects = [
-            f for f in model._meta.get_fields()
-            if (f.one_to_many or f.one_to_one)
-            and f.auto_created and not f.concrete
+            f for f in model._meta.get_fields() if
+            (f.one_to_many or f.one_to_one) and
+            f.auto_created and not f.concrete
         ]
         for rel in related_objects:
             if not rel.one_to_one:
