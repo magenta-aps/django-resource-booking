c57cc7bb8db8ac0cc0154c8491e1bfc912b74677
Søren Howe Gersager <thesyre@pm.me>
2019-02-11 12:01:40 +0100

    get_all_related_objects deprecated, now use list comprehension

--- booking/utils.py
+++ booking/utils.py
@@ -60,7 +60,12 @@ def get_related_content_types(model):
 
         types = [ContentType.objects.get_for_model(model)]
 
-        for rel in model._meta.get_all_related_objects():
+        related_objects = [
+            f for f in model._meta.get_fields()
+            if (f.one_to_many or f.one_to_one)
+            and f.auto_created and not f.concrete
+        ]
+        for rel in related_objects:
             if not rel.one_to_one:
                 continue
 
