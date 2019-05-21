5757dbe5152639b62219e5a9e78b44d349e69f36
Lars Peter Thomsen <larsp@magenta.dk>
2019-05-21 13:40:45 +0200

    Squash migrations

--- profile/migrations/0001_initial.py
+++ profile/migrations/0001_initial.py
@@ -2,7 +2,11 @@
 from __future__ import unicode_literals
 
 from django.db import migrations, models
+import datetime
+import django.db.models.deletion
+import django.utils.timezone
 from django.conf import settings
+import uuid
 
 
 class Migration(migrations.Migration):
@@ -14,10 +18,23 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
+            name='EmailLoginURL',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('uuid', models.UUIDField(default=uuid.uuid4)),
+                ('success_url', models.CharField(max_length=2024)),
+                ('created', models.DateTimeField(default=django.utils.timezone.now)),
+                ('expires_in', models.DurationField(default=datetime.timedelta(2))),
+                ('user', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
+            ],
+        ),
+        migrations.CreateModel(
             name='UserProfile',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('unit', models.ForeignKey(blank=True, to='booking.Unit', null=True)),
+                ('additional_information', models.TextField(default=b'', verbose_name='Yderligere information', blank=True)),
+                ('my_resources', models.ManyToManyField(to='booking.Product', verbose_name='Mine tilbud', blank=True)),
+                ('organizationalunit', models.ForeignKey(on_delete=django.db.models.deletion.SET_NULL, blank=True, to='booking.OrganizationalUnit', null=True)),
                 ('user', models.OneToOneField(to=settings.AUTH_USER_MODEL)),
             ],
         ),
@@ -25,10 +42,14 @@ class Migration(migrations.Migration):
             name='UserRole',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('role', models.IntegerField(default=0, choices=[(0, 'Underviser'), (1, 'V\xe6rt'), (2, 'Koordinator'), (3, 'Underviser')])),
+                ('role', models.IntegerField(default=0, choices=[(0, 'Underviser'), (1, 'V\xe6rt'), (2, 'Koordinator'), (3, 'Administrator'), (4, 'Fakultetsredakt\xf8r'), (5, 'Ingen')])),
                 ('name', models.CharField(max_length=256, blank=True)),
                 ('description', models.TextField(blank=True)),
             ],
+            options={
+                'verbose_name': 'brugerrolle',
+                'verbose_name_plural': 'brugerroller',
+            },
         ),
         migrations.AddField(
             model_name='userprofile',
