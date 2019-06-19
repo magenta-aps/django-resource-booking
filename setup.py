e1eb973644f2dcfcbc4bde5bb4b350d13bc17638
Lars Peter Thomsen <lapeth@gmail.com>
2019-06-19 11:00:00 +0200

    Preliminary conversion - updating dependencies and virtualenv

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-version = '0.1'
+version = '0.2'
 
 setup(name='django_resource_booking',
       version=version,
@@ -21,22 +21,22 @@ designed for the University of Copenhagen.""",
           # -*- Extra requirements: -*-
           'Django==1.11.20',
           'flake8==2.5.0',
-          'psycopg2',
+          'psycopg2-binary',
           'django-npm==1.0.0',
-          'django-recurrence==1.8.2',
+          'django-recurrence==1.9.0',
           'django-tinymce==2.8.0',
-          'djangosaml2==0.13.0',
+          'djangosaml2==0.17.2',
           'django-cron==0.5.1',
           'django-ckeditor==5.6.1',
           'django-macros==0.4.0',
           'djorm-ext-pgfulltext==0.10',
           'requests==2.20.0',
-          'django-debug-toolbar==1.9',
-          'django-hijack',
+          'django-debug-toolbar==1.11',
+          'django-hijack==2.1.10',
           'beautifulsoup4==4.7.1',
-          'Pillow',
-          'django-extensions==2.1.5',
-          'ipython==5.8.0'
+          'Pillow==6.0.0',
+          'django-extensions==2.1.9',
+          'ipython==7.5.0'
       ],
       entry_points="""
       # -*- Entry points: -*-
