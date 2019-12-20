fa4a563b8688d06dfe58bfc7c6b413a62ce8dc66
Lars Peter Thomsen <larsp@magenta.dk>
2019-12-20 13:00:18 +0100

    Bump django version

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ designed for the University of Copenhagen.""",
     zip_safe=False,
     install_requires=[
         # -*- Extra requirements: -*-
-        "Django==1.11.22",
+        "Django==1.11.27",
         "flake8==2.5.0",
         "psycopg2",
         "django-npm==1.0.0",
