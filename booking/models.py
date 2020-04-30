9e32971ca9f73be02370cdfe2708e4d769f5941b
Søren Howe Gersager <syrelyre@gmail.com>
2020-04-30 13:03:39 +0200

    add anonymization to KUEmailMessage body and htmlbody, update consent template to reflect anonymization after 2 years, update anonymization limit to 2 years

--- booking/models.py
+++ booking/models.py
@@ -6201,6 +6201,8 @@ class KUEmailMessage(models.Model):
 
     def anonymize(self):
         self.recipients = KUEmailMessage.anonymized
+        self.body = KUEmailMessage.anonymized
+        self.htmlbody = KUEmailMessage.anonymized
         self.save()
 
 
